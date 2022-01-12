fa3d4a83fe321060d529d77dc9cca17148c6f335
OliverSoucc <oliv572v@easv365.dk>
2022-01-12 11:23:22 +0100

    kk

--- src/gui/controller/MainPageController.java
+++ src/gui/controller/MainPageController.java
@@ -1,20 +1,40 @@
 package gui.controller;
 
+import be.Movie;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainPageController implements Initializable {
+    @FXML
+    public TableColumn<Movie, String> nameColumn;
+    @FXML
+    public TableColumn<Movie, String> ratingColumn;
+    @FXML
+    public TableColumn<Movie, String> category1Column;
+    @FXML
+    public TableColumn <Movie, String> category2Column;
+    @FXML
+    public TableColumn <Movie, String> category3Column;
+    @FXML
+    public TextField filter;
 
-    //public Button movieButton;
+    private final ObservableList <Movie> movieObservableList = FXCollections.observableArrayList();
 
     public void movieButtonOnAction(ActionEvent actionEvent) throws IOException {
         Parent root;
@@ -58,6 +78,10 @@ public class MainPageController implements Initializable {
     }
 
     private void tableView(){
-        
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        ratingColumn.setCellValueFactory(new PropertyValueFactory<>("rating"));
+        //category1Column.setCellValueFactory(new PropertyValueFactory<>(""));
+        //category2Column.setCellValueFactory(new PropertyValueFactory<>(""));
+        //category3Column.setCellValueFactory(new PropertyValueFactory<>(""));
     }
 }
