8a389c2a5242dc103f01a9e7d52a2d4be09b069c
pelonidas <janx803y@easv365.dk>
2022-01-15 21:11:27 +0100

    change title

--- src/GUI/View/Main.java
+++ src/GUI/View/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
-        primaryStage.setTitle("myTunes");
+        primaryStage.setTitle("MovieCollection");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
